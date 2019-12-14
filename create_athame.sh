5cc568aa269b470d2df338f7ecad346e68712113
Raphael Poli <39162515+RaphaelPoli@users.noreply.github.com>
2019-12-14 13:33:33 +0100

    Add files via upload

---
+++ create_athame.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#laucher.sh
+#sudo -H python3.5 -m pip --default-timeout=600 install pyserial
+#sudo -H python3.5 -m pip install --upgrade pip
+#sudo -H python3.5 -m pip install Cython
+sudo -H python3.5 -m pip install scikit-learn
+#sudo -H python3.5 -m pip --default-timeout=600  install pyexcel
+#sudo -H python3.5 -m pip --default-timeout=600  install pyexcel-ods
+#sudo -H python3.5 -m pip --default-timeout=600  install pyexcel-xls
